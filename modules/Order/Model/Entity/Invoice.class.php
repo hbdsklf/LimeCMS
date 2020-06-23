8a469bd08f0fe67b1e5d93f3687c88b1fa314f45
Thomas Wirz <thomas.daeppen@cloudrexx.com>
2020-06-23 10:56:47 +0200

    Revert "fix(order): fix whitespaces (CLX-749)"
    This reverts commit 19e2f55d4d1f71ba103a6cfaf3c9edc1f0258201.

--- modules/Order/Model/Entity/Invoice.class.php
+++ modules/Order/Model/Entity/Invoice.class.php
@@ -40,7 +40,7 @@ class InvoiceException extends \Exception {}
 
 /**
  * Class Invoice
- *
+ * 
  * @copyright   CLOUDREXX CMS - CLOUDREXX AG
  * @author      Project Team SS4U <info@cloudrexx.com>
  * @package     cloudrexx
@@ -52,29 +52,29 @@ class Invoice extends \Cx\Model\Base\EntityBase {
      * @var integer $id
      */
     protected $id;
-
+    
     /**
      * @var Cx\Modules\Order\Model\Entity\Order
      */
     protected $order;
-
+    
     /**
      * @var Cx\Modules\Order\Model\Entity\Payment
      */
     protected $payments;
-
+    
     /**
      * @var Cx\Modules\Order\Model\Entity\InvoiceItem
      */
     protected $invoiceItems;
-
-
+    
+    
     /**
      *
      * @var integer $paid
      */
     protected $paid;
-
+    
     /**
      * Constructor
      */
@@ -83,10 +83,10 @@ class Invoice extends \Cx\Model\Base\EntityBase {
         $this->invoiceItems = new \Doctrine\Common\Collections\ArrayCollection();
         $this->paid = false;
     }
-
+    
     /**
      * Get the id
-     *
+     * 
      * @return integer $id
      */
     public function getId() {
@@ -103,89 +103,89 @@ class Invoice extends \Cx\Model\Base\EntityBase {
 
     /**
      * Get the order
-     *
+     * 
      * @return \Cx\Modules\Order\Model\Entity\Order $order
      */
     public function getOrder() {
         return $this->order;
     }
-
+    
     /**
      * Set the order
-     *
+     * 
      * @param \Cx\Modules\Order\Model\Entity\Order $order
      */
     public function setOrder(Order $order) {
         $this->order = $order;
     }
-
+    
     /**
      * Add the payment
-     *
+     * 
      * @param \Cx\Modules\Order\Model\Entity\Payment $payment
      */
     public function addPayment(Payment $payment) {
         $payment->setInvoice($this);
         $this->payments[] = $payment;
-
+        
         if($payment->getAmount() == $this->getAmount()) {
             $this->paid = true;
         }
-
+        
         if($payment->getAmount() > $this->getAmount()) {
             throw new InvoiceException('Amount of payment must not be greater than invoice amount');
         }
-
+        
     }
-
+    
     /**
-     *
+     * 
      * @return \Cx\Modules\Order\Model\Entity\Payment $payments
      */
     public function getPayments() {
         return $this->payments;
     }
-
+    
     /**
      * Set the payment
-     *
+     * 
      * @param object $payments
      */
     public function setPayments($payments) {
         $this->payments = $payments;
     }
-
+    
     /**
      * Get the invoiceItems
-     *
+     * 
      * @return \Cx\Modules\Order\Model\Entity\InvoiceItem $invoiceItems
      */
     public function getInvoiceItems() {
         return $this->invoiceItems;
     }
-
+    
     /**
      * Set the invoiceItems
-     *
+     * 
      * @param object $invoiceItems
      */
     public function setInvoiceItems($invoiceItems) {
         $this->invoiceItems = $invoiceItems;
     }
-
+    
     /**
      * Add the invoiceItem
-     *
+     * 
      * @param \Cx\Modules\Order\Model\Entity\InvoiceItem $invoiceItem
      */
     public function addInvoiceItem(InvoiceItem $invoiceItem) {
         $invoiceItem->setInvoice($this);
         $this->invoiceItems[] = $invoiceItem;
     }
-
+    
     /**
      * Get Sum of all the associated \Cx\Modules\Order\Model\Entity\InvoiceItem::$price with VAT
-     *
+     * 
      * @return decimal the sum of all associated \Cx\Modules\Order\Model\Entity\InvoiceItem::$price with VAT
      */
     public function getAmount() {
@@ -193,18 +193,18 @@ class Invoice extends \Cx\Model\Base\EntityBase {
         $isReseller = $this->isReseller();
 
         foreach($this->invoiceItems as $invoiceItem) {
-            $totalInvoiceItemPrice += $invoiceItem->getPrice() + (
-                $isReseller
-                    ? 0.00
-                    : ($invoiceItem->getPrice() * $invoiceItem->getVatRate() * 0.01)
-            );
+            $totalInvoiceItemPrice += $invoiceItem->getPrice() +
+                                        ($isReseller
+                                            ? 0.00
+                                            : ($invoiceItem->getPrice() * $invoiceItem->getVatRate() * 0.01)
+                                        );
         }
         return $totalInvoiceItemPrice;
     }
-
+    
     /**
      * Get the paid
-     *
+     * 
      * @return boolean $paid
      */
     public function getPaid() {
