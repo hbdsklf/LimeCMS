b840626003f317ecd34e2ec0cbc3c5ea1c23059c
Thomas Wirz <thomas.daeppen@cloudrexx.com>
2020-07-21 17:10:01 +0200

    fix(datasource): make "0" compound id parts work again (CLX-3426)
    Revert "fix(datasource): ignore empty ids (CLX-2455)"
    
    This reverts commit d3afcad1eb5161ebfbb6d23a462fdb196230cd02.

--- core/DataSource/Model/Entity/DoctrineRepository.class.php
+++ core/DataSource/Model/Entity/DoctrineRepository.class.php
@@ -140,9 +140,6 @@ class DoctrineRepository extends DataSource {
         // Add id to filter (after other filters to prevent override)
         if (isset($elementId) && count($elementId)) {
             foreach ($elementId as $field=>$id) {
-                if (empty($id)) {
-                    continue;
-                }
                 $criteria[$field] = array('eq' => $id);
             }
         }
